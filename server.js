d9e0ba9cfbccd688103716f9469d8fe255025fdb
DistantDig <jameswaller711@gmail.com>
2023-10-27 16:49:38 -0700

    added the get and post processes for reading and recording notes

--- server.js
+++ server.js
@@ -1,20 +1,86 @@
 // Required dependancies for the application
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const util = require('util');
 
 const PORT = 3000;
 const app = express();
 
-// Redirect to index.html
+let jsonArray;
+
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+
+// Redirecting to files
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
-
-// Redirect to notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+
+// Functions to Read, Write and Append
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+  };
+
+
+// Requests
+app.get('/api/notes', (req, res) => {
+    console.log(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: 0
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in saving note');
+    }
+});
+
+
+// Listener
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
 });
